d03c7fc17fb8ea95ea148e434d35a4b566c0a4db
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-04-12 23:26:08 +0530

    add solution of maximum subarray sum of size k

---
+++ dsa/array_hashing/sliding_window/MaxSubArraySumK.java
@@ -0,0 +1,34 @@
+package dsa.array_hashing.sliding_window;
+
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+        int windowSum = 0;
+        int ans = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            windowSum += arr[i];
+
+            if (i >= k - 1) {
+                // we get the min required window size
+                ans = Math.max(ans, windowSum);
+                // shrink the left window part
+
+                windowSum -= arr[i - k + 1];
+            }
+        }
+        return ans;
+
+    }
+}
+
+
+public class MaxSubArraySumK {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int arr[] = {100, 200, 300, 400};
+        int res = sol.maxSubarraySum(arr, 2);
+        System.out.println("ans=" + res); // 700
+    }
+
+}
