6854843864f8ace5e54b6b9f23c621697c7a31ec
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-04-12 23:39:49 +0530

    add clean solution of maxSubarraySum of k

--- dsa/array_hashing/sliding_window/MaxSubArraySumK.java
+++ dsa/array_hashing/sliding_window/MaxSubArraySumK.java
@@ -20,6 +20,31 @@ class Solution {
         return ans;
 
     }
+
+    public int maxSubarraySumClean(int[] arr, int k) {
+        if (arr.length < k) { // not possible
+            return 0;
+        }
+
+        int windowSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            // get first window of size k
+            windowSum += arr[i];
+
+        }
+        int ans = windowSum; // atleast one we got
+
+        // now traverse from kth index till end of the array and maintain the window sum by adding
+        // next element and removing last from window
+
+        for (int i = k; i < arr.length; i++) {
+            windowSum += arr[i] - arr[i - k];// add next but remove last from window from left
+            ans = Math.max(ans, windowSum);
+        }
+
+        return ans;
+    }
 }
 
 
@@ -28,7 +53,12 @@ public class MaxSubArraySumK {
         Solution sol = new Solution();
         int arr[] = {100, 200, 300, 400};
         int res = sol.maxSubarraySum(arr, 2);
+
         System.out.println("ans=" + res); // 700
+        res = sol.maxSubarraySumClean(arr, 2);
+        System.out.println("ans=" + res); // 700
+
+
     }
 
 }
