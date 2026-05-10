c3ba5a8b8849cc88e4d5081e617f4ab2f77924c5
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-05-10 17:16:58 +0530

    add solution of ceil in other way than current

--- dsa/array_hashing/binary_search/CeilElement.java
+++ dsa/array_hashing/binary_search/CeilElement.java
@@ -21,6 +21,30 @@ class Solution {
         }
         return ans;
     }
+
+    public int findCeilWayTwo(int[] arr, int target) {
+        // code here
+        int n = arr.length;
+
+        int low = 0, high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= target) {
+
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        if (low >= n)
+            return -1; // means none of element is in array
+        return low;
+    }
 }
 
 
